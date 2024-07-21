750bb6f897ceed2178773a80db3212340d542fd4
rinatagliulov <rinat.agliulov@rentready.com>
2024-07-21 08:04:17 -0400

    Start Work.

---
+++ src/main/java/shelter/bot/botshelter/BotForTheShelterApplication.java
@@ -0,0 +1,13 @@
+package shelter.bot.botshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotForTheShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotForTheShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shelter/bot/botshelter/BotForTheShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package shelter.bot.botshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotForTheShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
