1f47b397e2609644801e46a14a120efe0a26aa48
Pupuskjaer <khasanovtimur2001@gmail.com>
2024-08-24 18:09:04 +0400

    Тут ничего не работает

--- src/main/java/shelter/bot/botshelter/services/CommandHandlerService.java
+++ src/main/java/shelter/bot/botshelter/services/CommandHandlerService.java
@@ -10,32 +10,29 @@ import shelter.bot.botshelter.configuration.TelegramBotConfiguration;
 import shelter.bot.botshelter.listener.BotListener;
 import shelter.bot.botshelter.model.Client;
 import shelter.bot.botshelter.model.Menu;
+import shelter.bot.botshelter.model.Shelter;
 import shelter.bot.botshelter.model.Volunteer;
+import shelter.bot.botshelter.services.intefaces.CommandHandler;
 
 import java.util.List;
 import java.util.Optional;
 
-import static shelter.bot.botshelter.constants.Commands.*;
 import static shelter.bot.botshelter.constants.Texts.ASKING_TO_CHOOSE;
 import static shelter.bot.botshelter.constants.Texts.NOT_FOUND;
 
 @Service
-public class CommandHandlerService {
+public class CommandHandlerService implements CommandHandler {
 
     private Logger logger = LoggerFactory.getLogger(BotListener.class);
 
     private final TelegramBot bot;
     private final Menu menu;
-    private ClientService clientService;
-    private VolunteerService volunteerService;
+    private final ClientService clientService;
+    private final VolunteerService volunteerService;
 
-    private TelegramBotConfiguration telegramBotConfiguration;
+    private final TelegramBotConfiguration telegramBotConfiguration;
 
-    public CommandHandlerService(TelegramBot bot,
-                                 Menu menu,
-                                 ClientService clientService,
-                                 VolunteerService volunteerService,
-                                 TelegramBotConfiguration telegramBotConfiguration) {
+    public CommandHandlerService(TelegramBot bot, Menu menu, ClientService clientService, VolunteerService volunteerService, TelegramBotConfiguration telegramBotConfiguration) {
         this.bot = bot;
         this.menu = menu;
         this.clientService = clientService;
@@ -44,41 +41,55 @@ public class CommandHandlerService {
     }
 
     public void handleCommand(Long chatId, String command) {
-        switch (command) {
-            case START_COMMAND:
-                Optional<Client> optional = clientService.findByChatId(chatId);
-                if (optional.isEmpty()) {
-                    clientService.saveClient(new Client(chatId));
-                    menu.sendMenu(chatId,
-                            telegramBotConfiguration.getStartMsg(),
-                            MAIN_MENU_COMMANDS, bot);
-                } else {
-                    menu.sendMenu(chatId, ASKING_TO_CHOOSE, SUBMENU_NEW_USER, bot);
-                }
-                ;
-                break;
-            case CALL_VOLUNTEER:
-                Optional<List<Volunteer>> optional1 = volunteerService.findAll();
+        if (checkContains(MAIN_MENU_COMMANDS,command)) {
+            handleMainCommands(chatId,command);
+        } else if (checkContains(SUBMENU_NEW_USER,command)) {
+            handleNewUserCommands(chatId,command);
+        } else if (checkContains(SUBMENU_CONSULTATION,command)) {
+            handleAdoptionCommands(chatId,command);
+        } else if (checkContains(SUBMENU_DOG_HANDLER,command)) {
+            handleDogHandlerCommands(chatId,command);
+        } else if (checkContains(SUBMENU_ADOPTION,command)) {
+            handleAdoptionCommands(chatId,command);
+        }else {
+            switch (command) {
+                case START_COMMAND:
+                    Optional<Client> optional = clientService.findByChatId(chatId);
+                    if (optional.isEmpty()) {
+                        clientService.saveClient(new Client(chatId));
+                        menu.sendMenu(chatId,
+                                telegramBotConfiguration.getStartMsg(),
+                                MAIN_MENU_COMMANDS, bot);
+                    } else {
+                        menu.sendMenu(chatId, ASKING_TO_CHOOSE, SUBMENU_NEW_USER, bot);
+                    }
+                    ;
+                    break;
+                case CALL_VOLUNTEER:
+                    Optional<List<Volunteer>> optional1 = volunteerService.findAll();
 
-                if (optional1.isPresent()) {
+                    if (optional1.isPresent()) {
 
-                    Optional<Volunteer> optionalVolunteer = getVolunteer();
-                    if (optionalVolunteer.isPresent()) {
-                        sendMessage(chatId, optionalVolunteer.get().toString());
+                        Optional<Volunteer> optionalVolunteer = getVolunteer();
+                        if (optionalVolunteer.isPresent()) {
+                            sendMessage(chatId, optionalVolunteer.get().toString());
+                        } else {
+                            sendMessage(chatId, "Волонтеры" + NOT_FOUND);
+                        }
+
                     } else {
                         sendMessage(chatId, "Волонтеры" + NOT_FOUND);
                     }
+                    break;
 
-                } else {
-                    sendMessage(chatId, "Волонтеры" + NOT_FOUND);
-                }
-                break;
-            default:
-                menu.sendMenu(chatId,
-                        telegramBotConfiguration.getStartMsg(),
-                        new String[]{START_COMMAND},
-                        bot);
+                default:
+                    menu.sendMenu(chatId,
+                            telegramBotConfiguration.getStartMsg(),
+                            new String[]{START_COMMAND},
+                            bot);
+            }
         }
+
     }
 
     private Optional<Volunteer> getVolunteer() {
@@ -95,6 +106,97 @@ public class CommandHandlerService {
         SendResponse response = bot.execute(sendMessage);
         if (!response.isOk()) {
             logger.error("Error during sending message: {}", response.description());
+        }
+    }
+
+    @Override
+    public void handleMainCommands(Long chatId, String command) {
+
+        switch(command) {
+            case MAIN_COMMAND1:
+
+                break;
+            case MAIN_COMMAND2:
+
+                break;
+
+            case MAIN_COMMAND3:
+
+                break;
+        }
+    }
+
+    @Override
+    public void handleNewUserCommands(Long chatId, String command) {
+
+        switch(command) {
+            case NEW_USER_COMMAND1:
+
+                break;
+            case NEW_USER_COMMAND2:
+
+                break;
+
+            case NEW_USER_COMMAND3:
+
+                break;
+        }
+    }
+
+    @Override
+    public void handleConsultationCommands(Long chatId, String command) {
+        switch(command) {
+            case CONSULTATION_COMMAND1:
+
+                break;
+            case CONSULTATION_COMMAND2:
+
+                break;
+
+            case CONSULTATION_COMMAND3:
+
+                break;
+            case CONSULTATION_COMMAND4:
+
+                break;
+        }
+    }
+
+    @Override
+    public void handleDogHandlerCommands(Long chatId, String command) {
+        switch(command) {
+            case DOG_HANDLER_COMMAND1:
+
+                break;
+            case DOG_HANDLER_COMMAND2:
+
+                break;
+
+            case DOG_HANDLER_COMMAND3:
+
+                break;
+
+        }
+    }
+
+    @Override
+    public void handleAdoptionCommands(Long chatId, String command) {
+        switch(command) {
+            case ADOPTION_COMMAND1:
+
+                break;
+            case ADOPTION_COMMAND2:
+
+                break;
+
+            case ADOPTION_COMMAND3:
+
+                break;
+
+            case ADOPTION_COMMAND4:
+
+                break;
+
         }
     }
 }
---
+++ src/main/resources/liquibase/scripts/animals.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset 1999aRf:2
+
+CREATE TABLE animals(
+id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  species TEXT NOT NULL,
+  shelter_id BIGINT NOT NULL
+);
